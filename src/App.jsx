18e0ee8ecfd82528fabccff8618c5dea8247b4d7
Alex Hamilton <alexhamilton-code@gmail.com>
2024-05-11 14:15:00 -0700

    PersonalInfo now gets pushed up to App.jsx and displayed properly. Adjusted buttons and functionality to compensate for pushing data to App.jsx. Added rudimentary CSS styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import PersonalInfo from './components/PersonalInfo'
+import './styles/style.css'
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: "",
+    email: "",
+    phone: ""
+  })
+
+  const handlePersonalInfoUpdate = (updatedInfo) => {
+    setPersonalInfo(updatedInfo);
+  }
 
   return (
-    <>
-      <PersonalInfo></PersonalInfo>
-    </>
+    <div id="container">
+      <div className='editBox'>
+      <PersonalInfo onChange={handlePersonalInfoUpdate}/>
+      </div>
+
+      <div>
+        <h1>{personalInfo.fullName}</h1>
+        <h2>{personalInfo.email}</h2>
+        <h3>{personalInfo.phone}</h3>
+      </div>
+    </div>
   )
 }
 
--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,40 +1,40 @@
 import { useState } from "react";
 
-function PersonalInfo() {
-const [personalInfo, setPersonalInfo] = useState({
-  fullName: "",
-  email: "",
-  phone: "",
-});
+function PersonalInfo({onChange}) {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+  });
+  //I felt it kind of dumb to automatically update the resume as you're typing, so I've implemented tempInfo so the resume is only updated when Submit is pressed
+  const [tempInfo, setTempInfo] = useState({
+    fullName: "",
+    email: "",
+    phone: ""
+  })
   
   const[editable, setEditable] = useState(false);
-  const[submittedInfo, setSubmittedInfo] = useState({});
-
-  const handleFullNameChange = (e) => {
-    setPersonalInfo(prevInfo => (
-      {...prevInfo, fullName: e.target.value}
-    ))
+  
+  //Lets you type in the textboxes
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setTempInfo((prevInfo) => ({...prevInfo, [name]: value}))
   }
-  const handleEmailChange = (e) => {
-    setPersonalInfo(prevInfo => (
-      {...prevInfo, email: e.target.value}
-    ))
-  }
-  const handlePhoneChange = (e) => {
-    setPersonalInfo(prevInfo => (
-      {...prevInfo, phone: e.target.value}
-    ))
-  }
-
+  //Makes the text boxes editable and pulls in the current values for personalInfo
   const handleEdit = () => {
     setEditable(true);
+    setTempInfo({...personalInfo});
   }
+  //Overwrites personalInfo with temp values and removes editability for text boxes
   const handleSubmit = () => {
-    setSubmittedInfo(personalInfo);
+    setPersonalInfo(tempInfo);
+    onChange(tempInfo);
     setEditable(false);
   }
-  const handleReset = () => {
-    setPersonalInfo(submittedInfo);
+  //Sets text box values back to last submitted (i.e. current personalInfo)
+  const handleCancel = () => {
+    setTempInfo({...personalInfo});
+    setEditable(false);
   }
   
   return(
@@ -42,29 +42,25 @@ const [personalInfo, setPersonalInfo] = useState({
       <h2>Personal Information</h2>
       <div>
         <label htmlFor="fullName">Full Name:</label>
-        <input type="text" id="fullName" name="fullName" value={personalInfo.fullName} onChange={handleFullNameChange} disabled={!editable}/>
+        <input
+          type="text" id="fullName" name="fullName" value={tempInfo.fullName} onChange={handleChange} disabled={!editable}/>
       </div>
       <div>
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" value={personalInfo.email} onChange={handleEmailChange} disabled={!editable}/>
+        <input type="text" id="email" name="email" value={tempInfo.email} onChange={handleChange} disabled={!editable}/>
       </div>
       <div>
         <label htmlFor="phone">Phone Number:</label>
-        <input type="text" id="phone" name="phone" value={personalInfo.phone} onChange={handlePhoneChange} disabled={!editable}/>
+        <input type="text" id="phone" name="phone" value={tempInfo.phone} onChange={handleChange} disabled={!editable}/>
       </div>
       {editable ? (
         <>
-          <button onClick={handleSubmit}>Save Changes</button>
-          <button onClick={handleReset}>Revert Changes</button>
+          <button className="btn" id="btnSubmit" onClick={handleSubmit}>Save</button>
+          <button className="btn" id="btnCancel" onClick={handleCancel}>Cancel</button>
         </>
       ): (
-        
-        <button onClick={handleEdit}>Edit</button>
-        
+        <button className="btn" id="btnEdit" onClick={handleEdit}>Edit</button>
       )}
-      <h1>{personalInfo.fullName}</h1>
-      <h2>{personalInfo.email}</h2>
-      <h3>{personalInfo.phone}</h3>
     </div>
   )
 }
